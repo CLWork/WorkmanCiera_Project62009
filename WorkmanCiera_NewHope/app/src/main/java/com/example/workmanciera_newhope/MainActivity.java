65a2f052a3bc5975cb839933f7fec704a740bb3b
CLWorkman <clworkman@student.fullsail.edu>
2020-09-02 18:00:04 -0600

    First Commit

---
+++ WorkmanCiera_NewHope/app/src/main/java/com/example/workmanciera_newhope/MainActivity.java
@@ -0,0 +1,18 @@
+//Ciera Workman
+//Project 6
+//MainActivity.java
+
+package com.example.workmanciera_newhope;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
