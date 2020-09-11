12f2770b40b71cc4270d6ddf8373da1f3c79ce27
CLWorkman <clworkman@student.fullsail.edu>
2020-09-10 22:33:45 -0600

    ADD: BottomNavBar and Layout files.

--- WorkmanCiera_NewHope/app/src/main/java/com/example/workmanciera_newhope/MainActivity.java
+++ WorkmanCiera_NewHope/app/src/main/java/com/example/workmanciera_newhope/MainActivity.java
@@ -4,15 +4,48 @@
 
 package com.example.workmanciera_newhope;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.navigationBar);
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.homePage:
+
+                return true;
+            case R.id.listPage:
+
+                return true;
+            case R.id.donatePage:
+
+                return true;
+
+            case R.id.aboutPage:
+
+                return true;
+
+            case R.id.settingPage:
+                
+                return true;
+        }
+        return false;
     }
 }
